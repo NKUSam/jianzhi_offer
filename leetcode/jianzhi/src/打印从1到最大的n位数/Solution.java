c6d37248006e7197a5044e1e7e00b995034bf227
Sam <1791694111@qq.com>
2021-02-18 12:16:07 +0800

    2/18

--- leetcode/jianzhi/src/打印从1到最大的n位数/Solution.java
+++ leetcode/jianzhi/src/打印从1到最大的n位数/Solution.java
@@ -1,5 +1,27 @@
 package 打印从1到最大的n位数;
 
 public class Solution {
+    //方法一：通过字符串相加得出最大值
+    public int[] printNumbers1(int n) {
+        String max_str = "";
+        while (n-- != 0){
+            max_str += "9";
+        }
+        int max = Integer.parseInt(max_str);
+        int[] result = new int[max];
+        for (int i = 0; i < max; i++) {
+            result[i] = i + 1;
+        }
+        return result;
+    }
 
+    //方法二：通过公式得出最大值
+    public int[] printNumbers2(int n) {
+        int max = (int) Math.pow(10, n) - 1;
+        int[] result = new int[max];
+        for (int i = 0; i < max; i++) {
+            result[i] = i + 1;
+        }
+        return result;
+    }
 }
